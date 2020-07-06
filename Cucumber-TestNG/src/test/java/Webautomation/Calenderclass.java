132a1f1a3bcfb83f20b77dc728dedc97cb6337d7
vicky274 <DELL@DESKTOP-B0O3T20.(none)>
2020-07-06 11:54:44 +0530

    new commits

--- Cucumber-TestNG/src/test/java/Webautomation/Calenderclass.java
+++ Cucumber-TestNG/src/test/java/Webautomation/Calenderclass.java
@@ -3,6 +3,7 @@ package Webautomation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -19,12 +20,15 @@ You can use the DateTimeFormatter class with the ofPattern() method in the
 same package to format or parse date-time objects. 
 The following example will remove both the "T" and milliseconds from the date-time:
 		 */
-		/*
+		
 		//local date
 		LocalDate myObj = LocalDate.now(); // Create a date object
 	    System.out.println(myObj); // Display the current date
 	    
-	    //format date
+	    LocalDateTime time = LocalDateTime.now();
+	    System.out.println(time);
+	    
+	   /* //format date
 		String s = "2020-01-25";
 
 		SimpleDateFormat s1 = new SimpleDateFormat("yyyy-MM-dd");
@@ -33,14 +37,14 @@ The following example will remove both the "T" and milliseconds from the date-ti
 
 		Date date = s1.parse(s);
 
-		System.out.println(s2.format(date));*/
-	
-		/*
-		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println(s2.format(date));
+	*/
+		
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
 	    Calendar c = Calendar.getInstance();
         c.add(Calendar.MONTH, +5);
         
-        System.out.println(format.format(c.getTime()));*/
+        System.out.println(format.format(c.getTime()));
         
      
 	}
